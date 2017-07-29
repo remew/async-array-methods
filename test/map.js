c2cfa653cbab27cccffaa3f70326bc69bc82d112
remew <foolishremew@gmail.com>
2017-07-29 20:45:23 +0900

    using sleep for check that mapAsync is enabled to use async/await

--- test/map.js
+++ test/map.js
@@ -1,12 +1,17 @@
 import test from 'ava';
 import mapAsync from '../lib/map';
 
+import sleep from '../test-util/sleep';
+
 test(async t => {
     const arr = [1, 2, 3, 4, 5];
 
     arr.mapAsync = mapAsync;
 
-    const ret = await arr.mapAsync(async n => n * 2);
+    const ret = await arr.mapAsync(async n => {
+        await sleep(10);
+        return n * 2
+    });
     t.deepEqual(ret, [
         2, 4, 6, 8, 10
     ]);
